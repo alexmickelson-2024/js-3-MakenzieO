fe49709e033ebf8ded640f5cd69ac61043505e4d
makenzie orme <makenzieorme@live.com>
2024-10-07 18:10:00 -0600

    added javascript

--- site.js
+++ site.js
@@ -1,3 +1,48 @@
 import { animals } from "./animals.js";
 
-console.log(animals[0]); // example showing you can use the animals array
+function buildAnimalCard(animal) {
+  const animalContent = `<div class="card">
+        <img src="${animal.image}" alt="${animal.title}" />
+        <div class="card-text">
+        <h1>${animal.title}</h1>
+        <p>${animal.description}</p>
+        </div>
+    </div>`;
+  return animalContent;
+}
+function buildAllCards() {
+  var cardsHTML = "";
+  const url = window.location.href;
+  var keyword = "";
+  var newList = animals;
+  if (url.includes("?")) {
+    const urlSplit = url.split("?");
+    var searchTerm = urlSplit[1].toLowerCase();
+    if (searchTerm.includes("filter")) {
+      keyword = searchTerm.split("=");
+    }
+    newList = filterCard(animals, keyword[1]);
+  }
+  newList.forEach((animal) => {
+    cardsHTML = cardsHTML + buildAnimalCard(animal);
+  });
+  return cardsHTML;
+}
+function filterTitleandDescription(cards, keyword) {
+  return (
+    cards.description.toLowerCase().includes(keyword) ||
+    cards.title.toLowerCase().includes(keyword)
+  );
+}
+function filterCard(cardList, keyword) {
+  const newList = cardList.filter((card) =>
+    filterTitleandDescription(card, keyword)
+  );
+  return newList;
+}
+function addAnimalCard(cards) {
+  const cardContainer = document.getElementById("card-container-id");
+  cardContainer.replaceChildren();
+  cardContainer.innerHTML = cards;
+}
+addAnimalCard(buildAllCards());
